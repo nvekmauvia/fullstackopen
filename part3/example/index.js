88cbb8074d4f362e55c7955cf031b0bf4fab46e9
nvekmauvia <nvekmauvia@gmail.com>
2024-01-31 16:30:52 +1000

    Added frontend from part2 to part3 backend

--- part3/example/index.js
+++ part3/example/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 let notes = [
     {
@@ -75,7 +78,7 @@ app.post('/api/notes', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
