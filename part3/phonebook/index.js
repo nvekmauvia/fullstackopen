13d0ccf7410d99f0ca3201ef09adb38d73b635ad
nvekmauvia <nvekmauvia@gmail.com>
2024-02-02 14:42:45 +1000

    3.13-3.18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,8 +6,8 @@ const app = express()
 const Person = require('./models/person')
 
 app.use(cors())
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -53,7 +53,7 @@ app.get('/api/people', (request, response) => {
     })
 })
 
-app.get('/api/people/:id', (request, response) => {
+app.get('/api/people/:id', (request, response, next) => {
     /*
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
@@ -62,16 +62,24 @@ app.get('/api/people/:id', (request, response) => {
     } else {
         response.status(404).end()
     }*/
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/people/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
 
-    response.status(204).end()
+app.delete('/api/people/:id', (request, response) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/people', (request, response) => {
@@ -104,12 +112,43 @@ app.post('/api/people', (request, response) => {
     })
 })
 
+app.put('/api/people/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
 const generateId = () => {
     const maxId = persons.length > 0
         ? Math.max(...persons.map(p => p.id))
         : 0
     return maxId + 1
 }
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, '0.0.0.0', function () {
