94d4d499b8bc3199e379ad676fad05a102044688
nvekmauvia <nvekmauvia@gmail.com>
2024-02-06 15:45:40 +1000

    4.1-4.2

--- part3/example/models/note.js
+++ part3/example/models/note.js
@@ -1,20 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
@@ -31,6 +16,5 @@ noteSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
-
 
 module.exports = mongoose.model('Note', noteSchema)
