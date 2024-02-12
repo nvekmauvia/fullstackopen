d7034ab023767d74b5412cec0e806cda1db678a8
nvekmauvia <nvekmauvia@gmail.com>
2024-02-12 16:10:45 +1000

    5.2-5.4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -5,15 +5,30 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [messageStyle, setMessageStyle] = useState('error')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [blogTitle, setBlogTitle] = useState('')
+  const [blogAuthor, setBlogAuthor] = useState('')
+  const [blogUrl, setBlogUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs(blogs)
     )
   }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -22,15 +37,73 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
-    } catch (exception) {
-      /*setErrorMessage('Wrong credentials')
+      setErrorMessage(`${username} Logged in`)
+      setMessageStyle('message')
       setTimeout(() => {
         setErrorMessage(null)
-      }, 5000)*/
+      }, 5000)
+
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setMessageStyle('error')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
     }
+  }
+
+  const handleCreateBlog = event => {
+    event.preventDefault()
+    try {
+      const blogObject = {
+        title: blogTitle,
+        author: blogAuthor,
+        url: blogUrl
+      }
+      blogService
+        .create(blogObject)
+        .then(responseData => {
+          setBlogs(blogs.concat(responseData))
+          setBlogTitle('')
+          setBlogAuthor('')
+          setBlogUrl('')
+          setErrorMessage(`a new blog ${blogObject.title} by ${blogObject.author} added`)
+          setMessageStyle('message')
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+        })
+    } catch (exception) {
+      setErrorMessage('Wrong format')
+      setMessageStyle('error')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    window.location.reload()
+  }
+
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+
+    return (
+      <div className={messageStyle}>
+        {message}
+      </div>
+    )
   }
 
   const loginForm = () => (
@@ -59,21 +132,57 @@ const App = () => {
 
   const blogsForm = () => (
     <div>
+      <p>{user.name} logged in <button onClickCapture={handleLogout}>logout</button>
+      </p>
+
+      <h2>create new</h2>
+      <form onSubmit={handleCreateBlog}>
+        <div>
+          title
+          <input
+            type="text"
+            value={blogTitle}
+            name="Title"
+            onChange={({ target }) => setBlogTitle(target.value)}
+          />
+        </div>
+        <div>
+          author
+          <input
+            type="text"
+            value={blogAuthor}
+            name="Author"
+            onChange={({ target }) => setBlogAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url
+          <input
+            type="text"
+            value={blogUrl}
+            name="Author"
+            onChange={({ target }) => setBlogUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+
+      <br />
+      <h2>blog list</h2>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+
     </div>
   )
 
   return (
     <div>
       <h2>blogs</h2>
+      <Notification message={errorMessage} />
       {!user && loginForm()}
-      {user && <div>
-        <p>{user.name} logged in</p>
-        {blogsForm()}
-      </div>
-      }
+      {user && blogsForm()}
     </div>
   )
 }
