38aea9a5b3421001931e877702d5d2e80c92a01b
nvekmauvia <nvekmauvia@gmail.com>
2024-02-26 17:27:54 +1000

    6.1-6.8

---
+++ part6/redux-anecdotes-main/src/components/AnecdoteList.jsx
@@ -0,0 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { vote } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({ anecdote, handleClick }) => {
+    return (
+        <div key={anecdote.id}>
+            <div>
+                {anecdote.content}
+            </div>
+            <div>
+                has {anecdote.votes}
+                <button onClick={handleClick}>vote</button>
+            </div>
+        </div>
+    )
+}
+
+const AnecdoteList = () => {
+    const dispatch = useDispatch()
+    const anecdotes = useSelector(state => state)
+    const sortedAnecdotes = anecdotes.slice().sort((a, b) => b.votes - a.votes)
+
+    return (
+        <div>
+            {sortedAnecdotes.map(anecdote =>
+                <Anecdote
+                    key={anecdote.id}
+                    anecdote={anecdote}
+                    handleClick={() =>
+                        dispatch(vote(anecdote.id))
+                    }
+                />
+            )}
+        </div>
+    )
+}
+
+export default AnecdoteList
---
+++ part6/redux-simple-notes/src/reducers/noteReducer.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//import App from './App.jsx'
+import { createStore } from 'redux'
+
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
+export const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id }
+  }
+}
+
+const noteReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'NEW_NOTE':
+      return [...state, action.payload]
+    case 'TOGGLE_IMPORTANCE': {
+      const id = action.payload.id
+      const noteToChange = state.find(n => n.id === id)
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
+      }
+      return state.map(note =>
+        note.id !== id ? note : changedNote
+      )
+    }
+    default:
+      return state
+  }
+}
+
+export default noteReducer 
